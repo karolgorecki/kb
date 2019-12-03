1dcbc0974c9deb9c8fa58bb9f2d2a0e91a50b38a
Karol Gorecki <2426927+karolgorecki@users.noreply.github.com>
2019-12-03 21:47:03 +0100

    Update menu

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -62,17 +62,14 @@ const Navbar = class extends React.Component {
               <Link className="navbar-item" to="/about">
                 About
               </Link>
-              <Link className="navbar-item" to="/products">
+              {/* <Link className="navbar-item" to="/products">
                 Products
-              </Link>
+              </Link> */}
               <Link className="navbar-item" to="/blog">
                 Blog
               </Link>
               <Link className="navbar-item" to="/contact">
                 Contact
-              </Link>
-              <Link className="navbar-item" to="/contact/examples">
-                Form Examples
               </Link>
             </div>
             {/* <div className="navbar-end has-text-centered">
